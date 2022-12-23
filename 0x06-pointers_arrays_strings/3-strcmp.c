495c4374ad5c0c2ae9a1fa08ca6a3f72ac4b0e68
Synergyfy <oyelakintobiloba@gmail.com>
2022-12-22 18:10:29 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,55 +1,22 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
-* infinite_add - adds two numbers
-* @n1: number one.
-* @n2: number two.
-* @r: buffer that the function will use to store the result.
-* @size_r: buffer size:
-* Return: the pointer to dest.
+* _strcmp - Function that compares two strings.
+* @s1: First string
+* @s2: Second string
+* Return: Value less than 0 if string is less than the other
+* Value greater than 0 if string is greater then the other
+* 0 if strings are equal
 */
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+int _strcmp(char *s1, char *s2)
 {
-	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
+	int counter, comVal;
 
-	while (*(n1 + c1) != '\0')
-		c1++;
-	while (*(n2 + c2) != '\0')
-		c2++;
-	if (c1 >= c2)
-		bg = c1;
-	else
-		bg = c2;
-	if (size_r <= bg + 1)
-		return (0);
-	r[bg + 1] = '\0';
-	c1--, c2--, size_r--;
-	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
-	while (bg >= 0)
+	counter = 0;
+	while (s1[counter] == s2[counter] && s1[counter] != '\0')
 	{
-		op = dr1 + dr2 + add;
-		if (op >= 10)
-			add = op / 10;
-		else
-			add = 0;
-		if (op > 0)
-			*(r + bg) = (op % 10) + 48;
-		else
-			*(r + bg) = '0';
-		if (c1 > 0)
-			c1--, dr1 = *(n1 + c1) - 48;
-		else
-			dr1 = 0;
-		if (c2 > 0)
-			c2--, dr2 = *(n2 + c2) - 48;
-		else
-			dr2 = 0;
-		bg--, size_r--;
+		counter++;
 	}
-	if (*(r) == '0')
-		return (r + 1);
-	else
-		return (r);
+	comVal = s1[counter] - s2[counter];
+	return (comVal);
 }
